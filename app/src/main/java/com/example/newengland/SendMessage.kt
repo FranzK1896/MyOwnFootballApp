3d13b8ceeff99ffa501c1b1fbefba69f53383ae1
FranzK1896 <franz1.kuerzinger@st.oth-regensburg.de>
2023-01-15 18:49:53 +0100

    Implemented a spinner from some nfl Teams When you click on the spinner you can choose a NFL team The Team you have choosen, there Email will be put in the Email account on the App You can send them direct an Email

--- app/src/main/java/com/example/newengland/SendMessage.kt
+++ app/src/main/java/com/example/newengland/SendMessage.kt
@@ -12,7 +12,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.newengland.databinding.ActivitySendMessageBinding
 
 
-class SendMessage : AppCompatActivity() {
+class SendMessage : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
     lateinit var email: String
     private lateinit var binding: ActivitySendMessageBinding
@@ -20,13 +20,93 @@ class SendMessage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = ActivitySendMessageBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+setContentView(binding.root)
         title = "Ticketstore"
 
 
+        val spinner: Spinner = findViewById(R.id.spinner2)
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.Clubs,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+            spinner.adapter = adapter
+            val spinner: Spinner = findViewById(R.id.spinner2)
+            spinner.onItemSelectedListener = this
+
+            spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View,
+                    position: Int,
+                    id: Long
+                ) {
+                    val selectedItem = parent.getItemAtPosition(position).toString()
+                    if(selectedItem== "Miami Dolphins")
+                    {
+                        email= "finstix@dolphins.com";
+                    }
+                    else if(selectedItem == "New England Patriots")
+                    {
+                        email = "tickets@patriots.com"
+                    }
+                    else if(selectedItem == "Buffalo Bills")
+                    {
+                        email = "tickets@buffalobills.com"
+                    }
+                    else if(selectedItem == "New York Jets")
+                    {
+                        email = "tickets@newyorkjets.com"
+                    }
+                    else if(selectedItem == "New York Giants")
+                    {
+                        email == "nyticket@giants.nfl.net"
+                    }
+                    else if(selectedItem == "Minnesota Vikings")
+                    {
+                        email = "customerservice@vikings.nfl.com"
+                    }
+
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {}
+            }
+            binding.Email.setOnClickListener()
+            {
+                sendHtmlEmail(email)
+            }
+
+        }
+
+
+    }
+
+
+
+
+
+
+    override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
+        // An item was selected. You can retrieve the selected item using
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("Not yet implemented")
+    }
+
+    fun sendHtmlEmail(email: String) {
+        val mailId = email
+        val emailIntent = Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", mailId, null))
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Subject text here")
+        emailIntent.putExtra(Intent.EXTRA_TEXT, Html.fromHtml("<p><b>Some Content</b></p>" +
+                "http://www.google.com" + "<small><p>More content</p></small>"))
+        startActivity(Intent.createChooser(emailIntent, "Send email..."))
     }
 }
-
 
 
 
